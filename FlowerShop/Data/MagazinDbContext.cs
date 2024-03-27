a36899ace5baca9c708de296f4bba66de97e2e18
Razvan-Ionut Cebotari <moraru.egor@elev.cihcahul.md>
2024-03-27 06:28:49 +0200

    AddFiles

---
+++ FlowerShop/Data/MagazinDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FlowerShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerShop.Data
+{
+    public class MagazinDbContext : DbContext
+    {
+        public MagazinDbContext(DbContextOptions<MagazinDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Produs> Prosuse { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
---
+++ FlowerShop/Models/Produs.cs
@@ -0,0 +1,12 @@
+﻿namespace FlowerShop.Models
+{
+    public class Produs
+    {
+        public int Id { get; set; }
+        public string Denumire { get; set; }
+        public double Pret { get; set; }
+        public string Descriere { get; set; }
+        public string Img { get; set; }
+        
+    }
+}
